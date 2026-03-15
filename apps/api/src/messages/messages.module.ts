38d735e97e4bb429bc620dee77e80f5a5058524c
Shadowscr-7 <jcg.software.solution@gmail.com>
2026-03-15 01:38:47 -0300

    fix: register BullMQ message queue in MessagesModule

--- apps/api/src/messages/messages.module.ts
+++ apps/api/src/messages/messages.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { BullModule } from "@nestjs/bullmq";
 import { MessagesService } from "./messages.service";
 import { MessagesController } from "./messages.controller";
 import { ChannelsModule } from "../channels/channels.module";
 
 @Module({
-  imports: [ChannelsModule],
+  imports: [BullModule.registerQueue({ name: "message" }), ChannelsModule],
   controllers: [MessagesController],
   providers: [MessagesService],
   exports: [MessagesService],
